46afc33312848840b90922be4eb4d6527d64ef62
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2019-01-22 09:21:35 +0300

    added json file

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let fs = require('fs');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var newsRouter = require('./routes/news');
+let indexRouter = require('./routes/index');
+let newsRouter = require('./routes/news');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('common', {
+  stream: fs.createWriteStream('./requests.log', {flags: 'a'})
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/news.js
+++ routes/news.js
@@ -1,63 +1,99 @@
-var articles = require('../entities/articles.js');
-var express = require('express');
-var router = express.Router();
+//let articles = require('../entities/articles.js');
+const filePath = './entities/articles.json';
+
+const fs = require('fs');
+const util = require('util');
+
+let express = require('express');
+let router = express.Router();
+
+let readFileAsync = util.promisify(fs.readFile);
+let writeFileAsync = util.promisify(fs.writeFile);
+
+
 
 /* GET news listing. */
-router.get('/', function(req, res, next) {
-  res.send(articles);
+router.get('/', async function (req, res, next) {
+    let articles = await getArticles();
+    res.send(articles);
 });
 
 /* GET articles listing. */
-router.get('/:id', function(req, res, next) {
-    let article=articles.filter(x=>x.id==req.params.id)[0];
-    if(!article) {
+router.get('/:id', async function (req, res, next) {
+    let articles = await getArticles();
+    let article = articles.filter(x => x.id == req.params.id)[0];
+    if (!article) {
         res.statusCode = 404;
-        return res.send({ error: 'Not found' });
+        return res.send({error: 'Not found'});
     }
     else {
-        return res.send({ status: 'OK', article:article });
+        return res.send({status: 'OK', article: article});
     }
-  });
+});
 
-  router.post('/', function(req, res, next) {
-    var article = {
+router.post('/', async function (req, res, next) {
+    let id = await getNewId();
+    let article = {
+        Id: id,
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
         url: req.body.images,
-        urlToImage:req.body.urlToImage
+        urlToImage: req.body.urlToImage
     };
+    let articles = await getArticles();
     articles.push(article);
-    return res.send({ status: 'OK', article:article });
-  });
+    await writeFileAsync(filePath, JSON.stringify({articles: articles, newId: ++id}));
 
-  router.put('/:id', function(req, res, next) {
-    let index=articles.findIndex(x=>x.id==req.params.id);
-    if(!index) {
+    return res.send({status: 'OK', article: article});
+});
+
+router.put('/:id', async function (req, res, next) {
+    let articles = await getArticles();
+    let index = articles.findIndex(x => x.id == req.params.id);
+    if (!index) {
         res.statusCode = 404;
-        return res.send({ error: 'Not found' });
+        return res.send({error: 'Not found'});
     }
     else {
-        articles[index]={
+        articles[index] = {
             title: req.body.title,
             author: req.body.author,
             description: req.body.description,
             url: req.body.images,
-            urlToImage:req.body.urlToImage
+            urlToImage: req.body.urlToImage
         };
-        return res.send({ status: 'OK', article:article });
+        await writeFileAsync(filePath, JSON.stringify({articles: articles, newId: getNewId()}));
+        return res.send({status: 'OK', article: article});
     }
-  });
+});
 
-  router.delete('/:id', function(req, res, next) {
-    articles=articles.filter(x=>x.id!==req.params.id);
-    if(!articles) {
+router.delete('/:id', async function (req, res, next) {
+    let articles = await getArticles();
+    let id = getNewId();
+    articles = articles.filter(x => x.id != req.params.id);
+    await writeFileAsync(filePath, JSON.stringify({articles: articles, newId: id}));
+    //return res.send({status: 'Delete OK', id: id});
+    if (!articles) {
         res.statusCode = 404;
-        return res.send({ error: 'Not found' });
+        return res.send({error: 'Not found'});
     }
     else {
-        return res.send({ status: 'OK', articles:articles });
+        return res.send({status: 'OK', articles: articles});
     }
-  });
-  
+
+});
+
+let getArticles = async function () {
+    let content = await readFileAsync(filePath);
+    let articles = JSON.parse(content).articles;
+    return articles;
+};
+
+let getNewId = async function () {
+    let content = await readFileAsync(filePath);
+    let newId = JSON.parse(content).newId;
+    return newId;
+};
+
 module.exports = router;
